6b94ceeebc94dc0e12d503312d58f7c8d1dce998
hayeojin <hayeojin4966@gmail.com>
2023-05-21 09:01:17 +0900

    maria db connection setup, install mariadb

---
+++ backend/database.js
@@ -0,0 +1,34 @@
+const mariadb = require("mariadb");
+const pool = mariadb.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "4966",
+  connectionLimit: 5,
+  database: "sentaku",
+});
+module.exports = {
+  async run(query, params) {
+    return new Promise((resolve, reject) => {
+      pool
+        .getConnection()
+        .then((conn) => {
+          conn
+            .query(query, params)
+            .then((rows) => {
+              resolve(rows);
+              conn.end(); // (필수) connection 종료
+            })
+            .catch((err) => {
+              console.log(err);
+              conn.end(); // (필수) connection 종료
+              reject(err);
+            });
+        })
+        .catch((err) => {
+          //not connected
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+};
