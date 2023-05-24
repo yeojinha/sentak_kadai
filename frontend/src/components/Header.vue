8c922dcac23dc61dff7be2ec90868c547ec64044
hayeojin <hayeojin4966@gmail.com>
2023-05-24 12:05:25 +0900

    Logout -> current user set to null, so on the MainList.vue 'show' finds there is no current user(login user checking by null or undefined).

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -228,7 +228,7 @@ export default {
         empty();
         printCurrentUser();
 
-        // window.location.reload(); //refresh cuz button doesn't refresh itself
+        window.location.reload(); //refresh cuz button doesn't refresh itself
       });
     };
     const checked = () => {
