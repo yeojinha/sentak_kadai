ed6da5b020c53a66a731c1ed1e31c33dd644e555
yeojinha <44031723+yeojinha@users.noreply.github.com>
2023-06-07 23:29:10 +0900

    Update Edit.vue

--- frontend/src/router/Edit.vue
+++ frontend/src/router/Edit.vue
@@ -10,8 +10,8 @@
           >
             <div class="card-body py-4 px-4 px-md-5">
               <p class="h1 text-center mt-3 mb-4 pb-3 text-primary">
-                <i class="fas fa-check-square me-1"></i>
-                <u>JOIN CHALLENGE</u>
+                <i class="fas fa-check-square me-1" style='color:mediumseagreen'></i>
+                <u style='color:mediumseagreen'>EDIT CHALLENGE</u>
               </p>
               <form>
                 <div class="form-group">
@@ -50,14 +50,24 @@
                     <button
                       type="button"
                       class="btn btn-primary"
+                       style="color: mediumseagreen"
                       @click="editItem"
                     >
                       edit
                     </button>
                   </router-link>
+                  <router-link to="/">
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                       style="color: mediumseagreen"
+                      @click="toHome"
+                    >
+                      HOME
+                    </button>
+                  </router-link>
                 </div>
               </form>
-              add challenge
             </div>
           </div>
         </div>
@@ -109,6 +119,14 @@ export default {
         return;
       }
     };
+    const toHome = () => {
+      state.confirm = window.confirm("Redirect to Home??");
+      if (state.confirm) {
+            router.push("/"); // Navigate to "/" route after alert OK button is clicked
+          }else{
+            return;
+          }
+      } 
 
     const directToError = () => {
       router.push({ path: "/notfound" });
@@ -134,7 +152,112 @@ export default {
       handleInput,
       router,
       directToError,
+      toHome,
     };
   },
 };
 </script>
+
+<style lang="scss">
+#list1 .form-control {
+  border-color: transparent;
+}
+#list1 .form-control:focus {
+  //when input textarea
+  border-color: rgb(20, 180, 92);
+  box-shadow: none;
+}
+
+#list1 .select-input.form-control[readonly]:not([disabled]) {
+  background-color: black;
+}
+
+.hide {
+  visibility: visible;
+}
+
+// <button class="button-11" role="button">Button 11</button>
+
+/* CSS */
+
+.form-group {
+  font-size: 20px;
+  height: 150%
+}
+
+.add-button {
+  // addbutton in form
+  transform: scale(1.3);
+  padding-left: 130px;
+}
+
+.card-body {
+  //text in card
+  font-size: 25px;
+
+}
+
+.btn {
+  //list button
+  border: solid thin rgb(20, 180, 92);
+  color: white;
+  background-color: white;
+}
+
+.btn:hover {
+  //mouseover list button
+  background-color: rgb(196, 236, 214);
+  border: rgb(20, 180, 92);
+}
+
+
+
+.card-header {
+  //all border of card
+  border: solid thin #eff1f2;
+  ;
+}
+
+.collapsearea {
+  //pulldown list view
+  border: solid thin #eff1f2;
+}
+
+.list-group-item {
+  color: black;
+}
+
+.showdownlist {
+  font-size: 20px;
+  color: black;
+}
+
+.linebetweenformandlist {
+  color: black;
+}
+
+
+
+
+.button-11 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 6px 14px;
+  font-family: -apple-system, BlinkMacSystemFont, "Roboto", sans-serif;
+  border-radius: 6px;
+  color: #000000;
+  background: #fff;
+  border: none;
+  box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-11:focus {
+  box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1),
+    0px 0px 0px 3.5px rgb(20, 180, 92);
+  outline: 0;
+}
+</style>
